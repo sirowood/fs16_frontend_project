10bf204d49ede8589ece914dd4657c2e6cb9227c
Xuefeng Wu <sirowood@gmail.com>
2023-10-11 10:04:37 +0300

    refactor code for better readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import { RouterProvider } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import { Toaster as ToasterProvider } from 'react-hot-toast';
 
+import store from './redux/store';
 import router from './router';
 import UserProvider from './providers/UserProvider';
 import ModalProvider from './providers/ModalProvider';
 
 const App = () => {
   return (
-    <UserProvider>
-      <RouterProvider router={router} />
-      <ModalProvider />
-    </UserProvider>
+    <ReduxProvider store={store}>
+      <UserProvider>
+        <RouterProvider router={router} />
+        <ModalProvider />
+        <ToasterProvider
+          toastOptions={{
+            style: {
+              background: '#333',
+              color: '#fff',
+            },
+          }}
+        />
+      </UserProvider>
+    </ReduxProvider>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { Toaster } from 'react-hot-toast';
 
 import App from './App';
-import store from './redux/store';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Toaster
-        toastOptions={{
-          style: {
-            background: '#333',
-            color: '#fff',
-          },
-        }}
-      />
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
